95aea7bf0200a54a8be3a7d3f4d90caefd048741
Megan Haazelager <Megan@MacBook-Pro-van-Megan.local>
2021-06-03 12:31:44 +0200

    create

---
+++ code/classes/connections.py
@@ -0,0 +1,6 @@
+class Connection:
+    def __init__(self, connection, duration):
+
+        # set variables
+        self.connection = connection
+        self.duration = duration
---
+++ code/main.py
@@ -0,0 +1,19 @@
+from classes.connections import Connection
+
+employeeList = list()
+employeeDict = {}
+
+f = open("/Users/Megan/reliable_railways/data/ConnectiesNationaal.csv")
+myLine = f.readline()
+print(myLine)
+while (len(myLine)>0):
+    connection_info = myLine.split(",")
+    connection = (connection_info[0], connection_info[1])
+    duration = connection_info[2]
+
+    print("connection_info: ", connection_info)
+    connections = {}
+    connections[connection] = Connection(connection, duration )
+    print(connections)
+    myLine = f.readline()
+f.close()
